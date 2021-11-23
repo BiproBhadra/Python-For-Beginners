2c6bf9538c7504244b891eb54e41faab53b65878
Bipro Bhadra <91278838+BiproBhadra@users.noreply.github.com>
2021-11-23 21:14:43 +0530

    Create Slicing in Array.py

---
+++ Slicing in Array.py
@@ -0,0 +1,16 @@
+import numpy as np
+a=[]
+size=int(input("State the Size of list for converting it into array:"))
+for i in range(size):
+    val=int(input("Enter Number:"))
+    a.append(val)
+print("Our List is=",a)    
+arr=np.array(a)
+for i in range(arr.size):
+    print(arr[i])
+sum=0
+for i in range(arr.size):
+    sum=sum+arr[i]
+print("Sum of array elements=",sum)  
+arrnew=arr[1:3]
+print(arrnew)
