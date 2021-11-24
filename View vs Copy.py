99f2fa4e24f4202a2b0ca673adbaed096b6012ee
Bipro Bhadra <91278838+BiproBhadra@users.noreply.github.com>
2021-11-24 18:48:13 +0530

    Create View vs Copy.py

---
+++ View vs Copy.py
@@ -0,0 +1,17 @@
+#View
+
+import numpy as np
+a=np.array([10,20,30,40,50,60,70,80,90])
+b=a[3:6]           #As a view we are getting all the elements here that's why any change will reflect in the array.
+b[:]=0
+print(b)
+print(a)
+
+#Copy
+
+import numpy as np
+a=np.array([10,20,30,40,50,60,70,80,90])
+b=a[3:6].copy()     #For using copy in slicing any changes won't reflect in the original array.        
+b[:]=0
+print(b)
+print(a)
