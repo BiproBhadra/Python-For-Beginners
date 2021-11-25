69b14cb34bc8a9a372c576af3aecd6b7f204b385
Bipro Bhadra <91278838+BiproBhadra@users.noreply.github.com>
2021-11-25 22:16:41 +0530

    Create Factorial of a number.py

---
+++ Factorial of a number.py
@@ -0,0 +1,8 @@
+import math
+
+def factorial(n):
+    return(math.factorial(n))
+
+num =int(input("Enter the number for which you want the factorial:"))
+print("Factorial of", num, "is",factorial(num))
+
