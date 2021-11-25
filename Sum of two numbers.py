94344fcc83d20c84f82ec916a30d72f86386bcf9
Bipro Bhadra <91278838+BiproBhadra@users.noreply.github.com>
2021-11-25 22:12:37 +0530

    Create Sum of two numbers.py

---
+++ Sum of two numbers.py
@@ -0,0 +1,9 @@
+# Python3 program to add two numbers
+
+number1 = input("First number: ")
+number2 = input("Second number: ")
+
+# User might also enter float numbers
+sum = float(number1) + float(number2)
+
+print("The sum of {0} and {1} is {2}" .format(number1, number2, sum))
