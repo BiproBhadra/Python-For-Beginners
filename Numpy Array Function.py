a3feb92901e14bbc912b01c771aac430f1ae88ec
Bipro Bhadra <91278838+BiproBhadra@users.noreply.github.com>
2021-11-28 20:11:53 +0530

    Create Numpy Array Function.py

---
+++ Numpy Array Function.py
@@ -0,0 +1,11 @@
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print(np.min(arr))
+print(np.max(arr))
+print(np.max(arr))
+print(np.argmin(arr))
+print(np.argmax(arr))
+print(np.sqrt(arr))
+print(np.sin(arr))
+print(np.cos(arr))
+
