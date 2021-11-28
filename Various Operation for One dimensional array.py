2de0ab698efa7a35ca4d086ccbee6b2f3503018d
Bipro Bhadra <91278838+BiproBhadra@users.noreply.github.com>
2021-11-28 19:27:49 +0530

    Create Various Operation for One dimensional array.py

---
+++ Various Operation for One dimensional array.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=np.arange(1,6)
+print("Multiplication:",arr*2)
+print("Summation:",arr+2)
+print("Exponent:",arr**3)       #In numpy array divide by zero gives infinity not any error.
+arr1=np.arange(1,6)
+arr2=np.arange(6,11)
+print("Sum of Arrays:",arr1+arr2)
