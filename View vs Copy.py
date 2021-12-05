41141b9961f375496acab527242bb040fd5b97c1
Bipro Bhadra <91278838+BiproBhadra@users.noreply.github.com>
2021-12-05 22:15:13 +0530

    Update View vs Copy.py

--- View vs Copy.py
+++ View vs Copy.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 a=np.array([10,20,30,40,50,60,70,80,90])
-b=a[3:6]           #As a view we are getting all the elements here that's why any change will reflect in the array.
+b=a[3:6]            #As a view we are getting all the elements here that's why any change will reflect in the array.
 b[:]=0
 print(b)
 print(a)
